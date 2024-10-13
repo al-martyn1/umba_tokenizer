5c9826c4cd599bddbf9a704a9666f7482c974ce2
Alexandr Martynov <mag@unicont.com>
2024-10-13 18:13:53 +0300

    Добил логи

--- inc/umba/tokenizer/tokenizer_log.h
+++ inc/umba/tokenizer/tokenizer_log.h
@@ -6,13 +6,17 @@
 
 #include "umba/umba.h"
 //
+#include "umba/simple_formatter.h"
+//
 #include "umba/rule_of_five.h"
 #include "umba/utility.h"
 #include "umba/text_position_info.h"
 #include "umba/format_message.h"
 #include "umba/escape_string.h"
 //
-#include "protogen-types.h"
+#include "umba/log.h"
+//
+// #include "protogen-types.h"
 
 //
 #include <string>
@@ -55,6 +59,11 @@ struct ParserErrorLog
 {
 
     ~ParserErrorLog() {}
+
+    using FormatArgsMapType = typename umba::FormatMessage<std::string>::macros_map_type;
+
+    FormatArgsMapType formatArgs; // Заполняется в парсере, используется при eventType == ParserErrorEventType::tokenError
+
 
     virtual
     void logErrorEvent( ParserErrorEventType          eventType
@@ -300,201 +309,6 @@ struct TokenizerTokenErrorHandler : public TokenizerErrorHandlerBase
 
 //----------------------------------------------------------------------------
 
-
-
-//----------------------------------------------------------------------------
-#define UMBA_TOKENIZER_LOG_CONLOG_SRC_FILE   srcFile ? srcFile : __FILE__
-#define UMBA_TOKENIZER_LOG_CONLOG_SRC_LINE   srcFile ? srcLine : __LINE__
-
-#define UMBA_TOKENIZER_LOG_CONLOG_ERR_INPUT                 umba::log::startLogError( umbaLogStreamErr, umba::log::LogEntryType::err ,  /* gopts, */  std::string()/*"err"*/, curFile.c_str(), lineNo, UMBA_TOKENIZER_LOG_CONLOG_SRC_FILE, UMBA_TOKENIZER_LOG_CONLOG_SRC_LINE )
-#define UMBA_TOKENIZER_LOG_CONLOG_ERR_INPUT_EX(errType)     umba::log::startLogError( umbaLogStreamErr, umba::log::LogEntryType::err ,  /* gopts, */  std::string(errType)  , curFile.c_str(), lineNo, UMBA_TOKENIZER_LOG_CONLOG_SRC_FILE, UMBA_TOKENIZER_LOG_CONLOG_SRC_LINE )
-#define UMBA_TOKENIZER_LOG_CONLOG_WARN_INPUT(warnType)      umba::log::startLogError( umbaLogStreamErr, umba::log::LogEntryType::warn,  /* gopts, */  std::string(warnType) , curFile.c_str(), lineNo, UMBA_TOKENIZER_LOG_CONLOG_SRC_FILE, UMBA_TOKENIZER_LOG_CONLOG_SRC_LINE )
-#define UMBA_TOKENIZER_LOG_CONLOG_INFO_INPUT(infoType)      umba::log::startLogError( umbaLogStreamErr, umba::log::LogEntryType::msg ,  /* gopts, */  std::string(infoType) , curFile.c_str(), lineNo, UMBA_TOKENIZER_LOG_CONLOG_SRC_FILE, UMBA_TOKENIZER_LOG_CONLOG_SRC_LINE )
-
-//----------------------------------------------------------------------------
-
-
-
-//----------------------------------------------------------------------------
-template<typename FilenameSetType>
-struct ParserConsoleErrorLog : public ParserErrorLog
-{
-
-    // TextPositionInfo
-    // umba_text_position_info_line_offset_type    lineOffset  ; //!< From data origin to line start
-    // umba_text_position_info_symbol_offset_type  symbolOffset; //!< From line start
-    // umba_text_position_info_symbol_offset_type  lineLen     ; //!< From start to first line end symbol or to end of text
-    // umba_text_position_info_line_number_type    lineNumber  ; //!< Zero based line number
-    // umba_text_position_info_file_id_type        fileId      ; //!< FileID
-
-    using SharedFilenameSetType = std::shared_ptr<FilenameSetType>;
-    using FileInfoType          = typename FilenameSetType::FileInfoType;
-
-    SharedFilenameSetType pFilenameSet;
-
-    ParserConsoleErrorLog(SharedFilenameSetType fnSet) : ParserErrorLog(), filenameSet(fnSet) {}
-
-
-    void logErrorEvent( ParserErrorEventType          eventType
-                      , const TextPositionInfo        &textPos
-                      , umba::tokenizer::payload_type payload
-                      , std::string                   erroneousValue     // Can be empty, can contain non-printable chars 
-                      , std::string                   erroneousLineText  // Can be empty, can contain non-printable chars 
-                      , const std::string             &customMsgId       // Can be empty
-                      , const std::string             &customMessage     // Can be empty
-                      , const char* srcFile,          int srcLine
-                      ) const
-                      override
-    {
-        erroneousValue = umba::escapeStringC(erroneousValue); // Тут не обязательно подсчитывать, сколько лишних символов добавилось
-
-        int lineNo = (int)textPos.lineNumber;
-
-        std::string curFile = "<->";
-
-        auto pFileInfo = pFilenameSet->getFileInfo(textPos.fileId);
-        UMBA_ASSERT(pFileInfo!=0);
-        curFile = pFileInfo->orgFilename;
-
-        auto errMarkerStr = std::string(erroneousLineText.size(), ' ');
-        if (textPos.symbolOffset>=errMarkerStr.size())
-            errMarkerStr.append(1,'^');
-        else
-            errMarkerStr[textPos.symbolOffset] = '^';
-        
-
-        if (!srcFile)
-        {
-            srcFile
-        }
-        
-                  int srcLine
-
-        switch(eventType)
-        {
-            case ParserErrorEventType::unexpected:
-            {
-                if (erroneousValue.empty())
-                {
-                    UMBA_TOKENIZER_LOG_CONLOG_ERR_INPUT_EX("unxp-symbol") << umba::FormatMessage("Unexpected symbol").toString() <<"\n";
-                }
-                else
-                {
-                    UMBA_TOKENIZER_LOG_CONLOG_ERR_INPUT_EX("unxp-symbol") << umba::FormatMessage("Unexpected symbol: \'$(Symbol)\'").arg("Symbol", erroneousValue).toString() <<"\n";
-                }
-
-
-                break;
-            }
-
-            case ParserErrorEventType::unknownOperator:
-            {
-                //std::cout << "Possible unknown operator: '" << umba::iterator::makeString(b, e) << "'\n";
-                break;
-            }
-
-            case ParserErrorEventType::stringLiteralWarning:
-            {
-                break;
-            }
-
-            case ParserErrorEventType::stringLiteralError:
-            {
-                break;
-            }
-
-            case ParserErrorEventType::tokenError:
-            {
-                break;
-            }
-
-            case ParserErrorEventType::customEvent:
-            {
-                break;
-            }
-
-        }
-
-        if (!erroneousLineText.empty() && !errMarkerStr.empty())
-        {
-            UMBA_TOKENIZER_LOG_CONLOG_ERR_INPUT << "Line: " << erroneousLineText << "\n";
-            UMBA_TOKENIZER_LOG_CONLOG_ERR_INPUT << "     |" << errMarkerStr      << "|\n";
-        }
-
-    }
-
-// template<typename StreamType, typename MsgType>
-// void printError(StreamType &ss, const std::string &inputFilename, umba::tokenizer::payload_type tokenType, umba::iterator::TextPositionCountingIterator<char> it, umba::iterator::TextPositionCountingIterator<char> itEnd, MsgType msg)
-// {
-//      UMBA_USED(tokenType);
-//  
-//      ss << "!!! ";
-//  
-//      if (it==itEnd)
-//      {
-//          ss << inputFilename << ": Unexpected end of file\n";
-//          ss << "TokenType: " << tokenType << "\n";
-//          return;
-//      }
-//  
-//      if (msg.empty())
-//      {
-//          msg = "Unexpected symbol";
-//      }
-//  
-//      auto errPos = it.getPosition(true); // с поиском конца строки (а вообще не надо пока, но пусть)
-//      std::string erroneousLineText = umba::iterator::makeString(it.getLineStartIterator(), it.getLineEndIterator());
-//      ss << inputFilename << ":" << errPos.toString<std::string>() << ": " << msg << "\n";
-//      ss << "Line: " << erroneousLineText << "\n";
-//      auto errMarkerStr = std::string(erroneousLineText.size(), ' ');
-//      if (errPos.symbolOffset>=errMarkerStr.size())
-//          errMarkerStr.append(1,'^');
-//      else
-//          errMarkerStr[errPos.symbolOffset] = '^';
-//      ss << "     |" << errMarkerStr << "|\n";
-//  
-//      if (it!=itEnd)
-//      {
-//          // char ch = *it;
-//          // ss << "ch: " << umba::escapeStringC(std::string(1,ch)) << "\n";
-//      }
-
-
-}; // struct ParserConsoleErrorLog
-
-
-// #define UMBA_LOG_ERR_INPUT                 umba::log::startLogError( umbaLogStreamErr, umba::log::LogEntryType::err ,  /* gopts, */  std::string()/*"err"*/, curFile.c_str(), lineNo, __FILE__, __LINE__ )
-// #define UMBA_LOG_ERR_INPUT_EX(errType)     umba::log::startLogError( umbaLogStreamErr, umba::log::LogEntryType::err ,  /* gopts, */  std::string(errType)  , curFile.c_str(), lineNo, __FILE__, __LINE__ )
-// #define UMBA_LOG_WARN_INPUT(warnType)      umba::log::startLogError( umbaLogStreamErr, umba::log::LogEntryType::warn,  /* gopts, */  std::string(warnType) , curFile.c_str(), lineNo, __FILE__, __LINE__ )
-// #define UMBA_LOG_INFO_INPUT(infoType)      umba::log::startLogError( umbaLogStreamErr, umba::log::LogEntryType::msg ,  /* gopts, */  std::string(infoType) , curFile.c_str(), lineNo, __FILE__, __LINE__ )
-//  
-// // options and other errors
-// #define UMBA_LOG_ERR                       umba::log::startLogError( umbaLogStreamErr, umba::log::LogEntryType::err ,  /* gopts,  */ std::string()/*"err"*/, (const char*)0 , 0     , __FILE__, __LINE__ )
-// #define UMBA_LOG_WARN(warnType)            umba::log::startLogError( umbaLogStreamErr, umba::log::LogEntryType::warn,  /* gopts,  */ std::string(warnType) , (const char*)0 , 0     , __FILE__, __LINE__ )
-// #define UMBA_LOG_INFO(infoType)            umba::log::startLogError( umbaLogStreamErr, umba::log::LogEntryType::msg ,  /* gopts,  */ std::string(infoType) , (const char*)0 , 0     , __FILE__, __LINE__ )
-//                                                                             /*         */
-// #define UMBA_LOG_MSG                       umba::log::startLogError( umbaLogStreamMsg, umba::log::LogEntryType::msg ,  /* gopts,  */ std::string()/*"msg"*/, (const char*)0 , 0     , (const char*)0, 0 )
-
-// enum class ParserErrorEventType
-// {
-//     customEvent
-//     unexpected
-//     unknownOperator // warning
-//     stringLiteralWarning
-//     stringLiteralError
-//     tokenError
-// };
-
-// #define LOG_ERR_INPUT                UMBA_LOG_ERR_INPUT
-// #define LOG_WARN_INPUT(warnType)     UMBA_LOG_WARN_INPUT(warnType)
-// #define LOG_INFO_INPUT(infoType)     UMBA_LOG_INFO_INPUT(infoType)
-//  
-// // options and other errors
-// #define LOG_ERR                      UMBA_LOG_ERR
-// #define LOG_WARN(warnType)           UMBA_LOG_WARN(warnType)
-// #define LOG_INFO(infoType)           UMBA_LOG_INFO(infoType)
-//  
-// #define LOG_MSG                      UMBA_LOG_MSG
 
 
 //----------------------------------------------------------------------------
---
+++ inc/umba/tokenizer/tokenizer_log_console.h
@@ -0,0 +1,174 @@
+/*! \file
+    \brief Подсистема протоколирования для токенизеров-парсеров
+ */
+
+#pragma once
+
+#include "tokenizer_log.h"
+//----------------------------------------------------------------------------
+
+
+
+//----------------------------------------------------------------------------
+// umba::tokenizer::log::
+namespace umba {
+namespace tokenizer {
+namespace log
+{
+
+//----------------------------------------------------------------------------
+
+
+
+//----------------------------------------------------------------------------
+#define UMBA_TOKENIZER_LOG_CONLOG_SRC_FILE   srcFile ? srcFile : __FILE__
+#define UMBA_TOKENIZER_LOG_CONLOG_SRC_LINE   srcFile ? srcLine : __LINE__
+
+#define UMBA_TOKENIZER_LOG_CONLOG_ERR_INPUT                 umba::log::startLogError( umbaLogStreamErr, umba::log::LogEntryType::err ,  /* gopts, */  std::string()/*"err"*/, curFile.c_str(), lineNo, UMBA_TOKENIZER_LOG_CONLOG_SRC_FILE, UMBA_TOKENIZER_LOG_CONLOG_SRC_LINE )
+#define UMBA_TOKENIZER_LOG_CONLOG_ERR_INPUT_EX(errType)     umba::log::startLogError( umbaLogStreamErr, umba::log::LogEntryType::err ,  /* gopts, */  std::string(errType)  , curFile.c_str(), lineNo, UMBA_TOKENIZER_LOG_CONLOG_SRC_FILE, UMBA_TOKENIZER_LOG_CONLOG_SRC_LINE )
+#define UMBA_TOKENIZER_LOG_CONLOG_WARN_INPUT(warnType)      umba::log::startLogError( umbaLogStreamErr, umba::log::LogEntryType::warn,  /* gopts, */  std::string(warnType) , curFile.c_str(), lineNo, UMBA_TOKENIZER_LOG_CONLOG_SRC_FILE, UMBA_TOKENIZER_LOG_CONLOG_SRC_LINE )
+#define UMBA_TOKENIZER_LOG_CONLOG_INFO_INPUT(infoType)      umba::log::startLogError( umbaLogStreamErr, umba::log::LogEntryType::msg ,  /* gopts, */  std::string(infoType) , curFile.c_str(), lineNo, UMBA_TOKENIZER_LOG_CONLOG_SRC_FILE, UMBA_TOKENIZER_LOG_CONLOG_SRC_LINE )
+
+//----------------------------------------------------------------------------
+
+
+
+//----------------------------------------------------------------------------
+template<typename FilenameSetType_>
+struct ParserConsoleErrorLog : public ParserErrorLog
+{
+
+    // TextPositionInfo
+    // umba_text_position_info_line_offset_type    lineOffset  ; //!< From data origin to line start
+    // umba_text_position_info_symbol_offset_type  symbolOffset; //!< From line start
+    // umba_text_position_info_symbol_offset_type  lineLen     ; //!< From start to first line end symbol or to end of text
+    // umba_text_position_info_line_number_type    lineNumber  ; //!< Zero based line number
+    // umba_text_position_info_file_id_type        fileId      ; //!< FileID
+
+    using FilenameSetType       = FilenameSetType_;
+    using SharedFilenameSetType = std::shared_ptr<FilenameSetType>;
+    using FileInfoType          = typename FilenameSetType::FileInfoType;
+
+    SharedFilenameSetType pFilenameSet;
+
+    ParserConsoleErrorLog(SharedFilenameSetType pFnSet) : ParserErrorLog(), pFilenameSet(pFnSet) {}
+
+
+    void logErrorEvent( ParserErrorEventType          eventType
+                      , const TextPositionInfo        &textPos
+                      , umba::tokenizer::payload_type payload
+                      , std::string                   erroneousValue     // Can be empty, can contain non-printable chars 
+                      , std::string                   erroneousLineText  // Can be empty, can contain non-printable chars 
+                      , const std::string             &customMsgId       // Can be empty
+                      , const std::string             &customMessage     // Can be empty
+                      , const char* srcFile,          int srcLine
+                      ) const
+                      override
+    {
+        erroneousValue = umba::escapeStringC(erroneousValue); // Тут не обязательно подсчитывать, сколько лишних символов добавилось
+
+        int lineNo = (int)textPos.lineNumber;
+
+        std::string curFile = "<->";
+
+        auto pFileInfo = pFilenameSet->getFileInfo(textPos.fileId);
+        UMBA_ASSERT(pFileInfo!=0);
+        curFile = pFileInfo->orgFilename;
+
+        using FormatMessage = umba::FormatMessage<std::string>;
+
+
+        switch(eventType)
+        {
+            case ParserErrorEventType::unexpected:
+            {
+                if (erroneousValue.empty())
+                {
+                    UMBA_TOKENIZER_LOG_CONLOG_ERR_INPUT_EX("unexpected-symbol") << FormatMessage("unexpected symbol").toString() <<"\n";
+                }
+                else
+                {
+                    UMBA_TOKENIZER_LOG_CONLOG_ERR_INPUT_EX("unexpected-symbol") << FormatMessage("unexpected symbol: \'$(Value)\'").arg("Value", erroneousValue).toString() <<"\n";
+                }
+
+
+                break;
+            }
+
+            case ParserErrorEventType::unknownOperator:
+            {
+                UMBA_TOKENIZER_LOG_CONLOG_ERR_INPUT_EX("unknown-operator") << FormatMessage("possible unknown operator: '$(Value)'").arg("Value", erroneousValue).toString() <<"\n";
+                //std::cout << "Possible unknown operator: '" << umba::iterator::makeString(b, e) << "'\n";
+                break;
+            }
+
+            case ParserErrorEventType::stringLiteralWarning:
+            {
+                UMBA_TOKENIZER_LOG_CONLOG_WARN_INPUT("string-literal") 
+                    << FormatMessage("string literal, suspicious character value: '$(Value)': $(Message)")
+                       .arg("Value", erroneousValue)
+                       .arg("Message", customMessage) // customMsgId - for translation
+                       .toString() <<"\n";
+                break;
+            }
+
+            case ParserErrorEventType::stringLiteralError:
+            {
+                UMBA_TOKENIZER_LOG_CONLOG_ERR_INPUT_EX("string-literal") 
+                    << FormatMessage("string literal, invalid character value: '$(Value)': $(Message)")
+                       .arg("Value", erroneousValue)
+                       .arg("Message", customMessage) // customMsgId - for translation
+                       .toString() <<"\n";
+                break;
+            }
+
+            case ParserErrorEventType::tokenError:
+            {
+                // Это сообщение обычно выводится, когда парсер возвращает false.
+                // Парсер должен установить formatArgs, и заполнить текст сообщения
+                std::string msgId = "token-error";
+                auto msgIdIt = formatArgs.find("msg-id");
+                if (msgIdIt!=formatArgs.end())
+                    msgId = msgIdIt->second;
+                UMBA_TOKENIZER_LOG_CONLOG_ERR_INPUT_EX(msgId)
+                    << FormatMessage(customMessage).values(formatArgs)
+                       .toString() <<"\n";
+                break;
+            }
+
+            case ParserErrorEventType::customEvent:
+            {
+                break;
+            }
+
+            default: erroneousLineText.clear(); // Доп инфу не надо выводить, так как нет основного сообщения
+                     
+        }
+
+        if (!erroneousLineText.empty())
+        {
+            std::size_t errPos = textPos.symbolOffset;
+            std::string escapedStr = umba::tokenizer::log::ParserErrorLog::escapeStringAndCorrectPos( erroneousLineText, errPos );
+            std::string markerStr  = umba::tokenizer::log::ParserErrorLog::makeMarkerString(escapedStr, errPos );
+
+            UMBA_TOKENIZER_LOG_CONLOG_ERR_INPUT << "Line :" << escapedStr << "\n";
+            UMBA_TOKENIZER_LOG_CONLOG_ERR_INPUT << "     |" << markerStr  << "|\n";
+        }
+
+    }
+
+
+}; // struct ParserConsoleErrorLog
+
+//----------------------------------------------------------------------------
+
+
+
+//----------------------------------------------------------------------------
+
+} // namespace log
+} // namespace tokenizer
+} // namespace umba
+
+
+
